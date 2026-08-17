63b0e0773a45fa1910ac56ff1bf1555969aa4661
Valentin Ganchev <viganchev6@abv.bg>
2026-08-17 03:58:53 +0300

    feat(friends): register FriendsModule; BlocksService.block hard-deletes graph edges

--- apps/backend/src/modules/blocks/blocks.module.ts
+++ apps/backend/src/modules/blocks/blocks.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 
+import { FriendsModule } from '../friends/friends.module';
 import { BlocksController } from './blocks.controller';
 import { BlocksService } from './blocks.service';
 
 @Module({
+  imports: [forwardRef(() => FriendsModule)],
   controllers: [BlocksController],
   providers: [BlocksService],
   exports: [BlocksService],
--- apps/backend/src/modules/friends/friends.module.ts
+++ apps/backend/src/modules/friends/friends.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 
 import { BlocksModule } from '../blocks/blocks.module';
 import { FriendsController } from './friends.controller';
 import { FriendsService } from './friends.service';
 
 @Module({
-  imports: [BlocksModule],
+  imports: [forwardRef(() => BlocksModule)],
   controllers: [FriendsController],
   providers: [FriendsService],
   exports: [FriendsService],
